90a8ade31e003d2c3a5ea23d62464298c60ceb38
Julien Dussart <julien.dussart@leikir.io>
2017-01-04 14:59:52 +0100

    clean override of find user from omniauth hash

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,29 +14,13 @@ module Users
     protected
 
     def get_resource_from_auth_hash
-      # We don't need that since we trust the email of unique provider google
-      # @resource = resource_class.where({
-      #                                    uid:      auth_hash['uid'],
-      #                                    provider: auth_hash['provider']
-      #                                  }).first
-
       @resource = resource_class.where(email: auth_hash['info']['email']).first
 
       # TODO : handle properly fail
+      # JD : not working, need to do something smarter if this method return nil
       if @resource.nil?
         redirect_to root_path, alert: "Unkown email!" and return
       end
-
-      # sync user info with provider, update/generate auth token
-      # assign_provider_attrs(@resource, auth_hash)
-
-      # @resource.uid = auth_hash['uid']
-      # @resource.provider = auth_hash['provider']
-      @resource.save
-
-      # assign any additional (whitelisted) attributes
-      # extra_params = whitelisted_params
-      # @resource.assign_attributes(extra_params) if extra_params
 
       @resource
     end
